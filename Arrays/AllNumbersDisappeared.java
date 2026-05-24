620845f2960c4bce0277ecc0b2d2ff7cd8e9813d
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-05-24 11:30:58 +0530

    all numbers disappeared

---
+++ Arrays/AllNumbersDisappeared.java
@@ -0,0 +1,24 @@
+package Arrays;
+import java.util.*;
+
+public class AllNumbersDisappeared{
+    public static void main(String args[]){
+        int nums[] = {4,3,2,8,8,2,3,1};
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i<nums.length; i++){
+            int index = Math.abs(nums[i]) - 1;
+
+            if(nums[index] > 0){
+                nums[index] = -nums[index];
+            }
+        }
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > 0){
+                result.add(i+1);
+            }
+        }
+        System.out.println(result);
+    }
+}

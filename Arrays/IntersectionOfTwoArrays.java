ea6354ab467269893dfd906426d5353c9a014e62
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-05-24 09:44:06 +0530

    new file

---
+++ Arrays/IntersectionOfTwoArrays.java
@@ -0,0 +1,23 @@
+package Arrays;
+import java.util.*;
+
+public class IntersectionOfTwoArrays {
+    public static void main(String args[]){
+    int nums1[] = {9,4,9,8,4};
+    int nums2[] = {4,9,5};
+
+    HashSet<Integer> set1 = new HashSet<>();
+    HashSet<Integer> result = new HashSet<>();
+
+    for(int num : nums1){
+        set1.add(num);
+    }
+
+    for(int num : nums2){
+        if(set1.contains(num)){
+            result.add(num);
+        }
+    }
+    System.out.println((result));
+}
+}

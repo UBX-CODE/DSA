ae1388d090e93ea2b6579e0869c2fd5d954b66ae
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-05-26 09:53:04 +0530

    gas station

---
+++ Arrays/GasStation.java
@@ -0,0 +1,10 @@
+//Leetcode Q.134
+package Arrays;
+import java.util.*;
+
+public class GasStation{
+    public static void main(String args[]){
+        int gas[] = {1,2,3,4,5};
+        int cost[] = {3,4,5,1,2};
+    }
+}

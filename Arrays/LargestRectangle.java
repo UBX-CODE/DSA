736e8ad4980afd6feb16e883f52f6d059378854e
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-05-27 12:31:51 +0530

    largest rectangle

---
+++ Arrays/LargestRectangle.java
@@ -0,0 +1,35 @@
+//Leetcode Q.84 
+package Arrays;
+import java.util.*;
+
+public class LargestRectangle {
+    public static void main(String args[]){
+        int heights[] = {2,1,5,6,2,3};
+
+        Stack<Integer> stack = new Stack<>();
+
+        int maxArea = 0;
+
+        for(int i=0;i<=heights.length;i++){
+
+            int currentHeight = (i==heights.length) ? 0 : heights[i];
+
+            while(!stack.isEmpty() && currentHeight < heights[stack.peek()]){
+
+                int height = heights[stack.pop()];
+
+                int width;
+                if(stack.isEmpty()){
+
+                    width=i;
+                }
+                else{
+                    width = i-stack.peek()-1;
+                }
+                maxArea = Math.max(maxArea,height * width);
+            }
+            stack.push(i);
+        }
+        System.out.println(maxArea);
+    }
+}

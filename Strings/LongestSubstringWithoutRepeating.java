0df85333c1fd8a4149e86f209b15f7b2763acacc
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-05-28 07:35:37 +0530

    Longest Substring Without Repeating

---
+++ Strings/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,26 @@
+//Leetcode Q.3
+package Strings;
+import java.util.*;
+
+public class LongestSubstringWithoutRepeating{
+    public static void main(String args[]){
+        String s = "abcabcbb";
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int maxLength = 0;
+
+        for(int right = 0; right < s.length(); right++){
+
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            };
+            set.add(s.charAt(right));
+
+            maxLength = Math.max(maxLength, right-left+1);
+        }
+        System.out.println(maxLength);
+    }
+}

c696d11ac8d4223086d61a2d670aada39a3143f3
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-06-02 10:04:45 +0530

    linked list started

---
+++ LinkedList/ReverseLL.java
@@ -0,0 +1,16 @@
+//Leetcode Q.206
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}

794f909ace23cf4e5ba04e59c4b088597b869041
UBX-CODE <ujjwalsharma1910@gmail.com>
2026-06-12 11:38:45 +0530

    Palindrome Partitioning

---
+++ Recursion/PalindromePartitioning.java
@@ -0,0 +1,89 @@
+//Leetcode Q.131
+package Recursion;
+import java.util.*;
+
+public class PalindromePartitioning {
+
+    static void backtrack(
+            int start,
+            String s,
+            List<String> current,
+            List<List<String>> result) {
+
+        if(start == s.length()) {
+
+            result.add(
+                    new ArrayList<>(current)
+            );
+
+            return;
+        }
+
+        for(int end=start;
+                end<s.length();
+                end++) {
+
+            if(isPalindrome(
+                    s,
+                    start,
+                    end
+            )) {
+
+                current.add(
+                        s.substring(
+                                start,
+                                end+1
+                        )
+                );
+
+                backtrack(
+                        end+1,
+                        s,
+                        current,
+                        result
+                );
+
+                current.remove(
+                        current.size()-1
+                );
+            }
+        }
+    }
+
+    static boolean isPalindrome(
+            String s,
+            int left,
+            int right) {
+
+        while(left < right) {
+
+            if(s.charAt(left)
+                    !=
+               s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "aab";
+
+        List<List<String>> result =
+                new ArrayList<>();
+
+        backtrack(
+                0,
+                s,
+                new ArrayList<>(),
+                result
+        );
+
+        System.out.println(result);
+    }
+}
